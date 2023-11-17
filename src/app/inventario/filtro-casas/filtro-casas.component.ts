56e34760ec6fc3d126b8f0af85c71332c3ef92d0
Janeth <janeth.montero@ontica.org>
2023-11-16 18:40:35 -0700

    clase 2: avance en componentes de inventario

--- src/app/inventario/filtro-casas/filtro-casas.component.ts
+++ src/app/inventario/filtro-casas/filtro-casas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filtro-casas',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./filtro-casas.component.scss']
 })
 export class FiltroCasasComponent {
+
+  @Input() mensaje: string = '';
 
 }
