229bedd959a7578dd4f028f8bc5f037654fdf354
Janeth <janeth.montero@ontica.org>
2023-11-23 13:30:58 -0700

    clase 6: config de branding + componente pagina no encontrada

--- src/app/pages/autenticacion/pagina-no-encontrada/pagina-no-encontrada.component.ts
+++ src/app/pages/autenticacion/pagina-no-encontrada/pagina-no-encontrada.component.ts
@@ -3,12 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pagina-no-encontrada',
   template: `
-    <p>
-      pagina-no-encontrada works!
-    </p>
+
+    <div class="not-found-container">
+
+      <img src="../../../../assets/img/not-found.jpg" alt="Imagen no encontrada" class="not-found-image">
+
+    </div>
+
   `,
-  styles: [
-  ]
+  styles: [`
+
+  .not-found-container {
+    width: 100%;
+    height: 75vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+  }
+
+  .not-found-image {
+    width: auto;
+    height: 100%;
+    object-fit: contain;
+    object-position: center;
+  }
+
+  `]
 })
 export class PaginaNoEncontradaComponent {
 
